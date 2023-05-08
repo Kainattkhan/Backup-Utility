d2cd07a6972c2b7ffc510c61222f5ecc5fcf2ed0
KainatIbrahim <kainatabrahim@gmail.com>
2023-05-08 15:44:08 +0500

    restore errors resolved

--- src/app/restorebackup/restorebackup.component.ts
+++ src/app/restorebackup/restorebackup.component.ts
@@ -13,6 +13,7 @@ export class RestorebackupComponent implements OnInit {
   selectAllChecked = false;
   tableRendered: boolean = false;
   dataShow:any;
+  checkedoptions: any[] = [];
 
 
   formatDate(date: Date): string {
@@ -27,41 +28,10 @@ export class RestorebackupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  selectAll() {
-    for (let item of this.filteredData) {
-      item.selected = !item.selected;
-    }
-  }
-
   onOptionSelected(option: string) {
     this.selectedOption = option;
   }
 
-  populateTable() {
-    if (this.selectedOption === 'option1') {
-      this.http.get(`http://localhost:8080/mongo/showBackup/${this.formatDate(new Date(this.selectedDate))}`).subscribe((data: any) => {
-        this.filteredData = Object.entries(data).map(([key, value]) => {
-          return {
-            name: key,
-            contents: value,
-            downloadLink: `http://localhost:8080/mongo/zip/${key}/${value}`
-          };
-        });
-      });
-      //contents = value.split(',');
-    } else if (this.selectedOption === 'option2') {
-      this.http.get(`http://localhost:8080/sql/showBackupFiles/${this.formatDate(new Date(this.selectedDate))}`).subscribe((data: any) => {
-        this.filteredData = Object.entries(data).map(([key]) => {
-          return {
-            name: key,
-            // contents: key,
-            downloadLink: `http://localhost:8080/sql/createzip/${key}`
-          };
-        });
-      });
-    }
-  }
-  
   sqlData(option: string){
     this.filteredData = [];
     this.selectedDate=''
@@ -75,7 +45,93 @@ export class RestorebackupComponent implements OnInit {
     this.tableRendered = false;  
     this.selectedOption = option;
   } 
+
+  selectAll() {
+    //checks wheather the selected property is true on each item or not
+    const allSelected = this.filteredData.every(item => item.selected);
+    
+    //assigns negation to the selected property of each item (if items are checked, it will uncheck and viceversa)
+    this.filteredData.forEach(item => item.selected = !allSelected);
+
+    if (allSelected) {
+      // If "select all" is unchecked, clear the checkedoptions array
+      this.checkedoptions = [];
+    } else {
+      // If "select all" is checked, add all items to the checkedoptions array
+      this.checkedoptions = [...this.filteredData];
+    }
+  }
+
+  updateSelectedItems() {
+    this.checkedoptions = this.filteredData.filter(item => item.selected);
+  }
+
+  populateTable() {
+    if (this.selectedOption === 'option1') {
+      this.http.get(`http://localhost:8080/mongo/showBackup/${this.formatDate(new Date(this.selectedDate))}`).subscribe((data: any) => {
+        this.filteredData = Object.entries(data).map(([key, value]) => {
+          return {
+            name: key,
+            contents: value,
+            downloadLink: `http://localhost:8080/mongo/zip/${key}/${value}`
+          };
+        });
+      });
+    } else if (this.selectedOption === 'option2') {
+      this.http.get(`http://localhost:8080/sql/showBackupFiles/${this.formatDate(new Date(this.selectedDate))}`).subscribe((data: any) => {
+        this.filteredData = Object.entries(data).map(([key,value]) => {
+          return {
+            name: key,
+            contents: value,
+            downloadLink: `http://localhost:8080/sql/createzip/${key}`
+          };
+        });
+      });
+    }
+  }
+
+  restoreData(){
+    this.checkedoptions.forEach(date => {
+      if (date.selected) {
+        if(this.selectedOption === "option1")
+          date.contents.forEach((type:string) =>{
+            this.http.get(`http://localhost:8080/mongo/restore/${date.name}/${type}`).subscribe(
+              (response) => {
+                const result= 
+                // handle success
+                console.log(response);
+              },
+              (error) => {
+                // handle error
+                console.log(error);
+              }
+            );
+          })
+
+          if(this.selectedOption === "option2")
+
+          date.contents.forEach((type:string) =>{
+            this.http.get(`http://localhost:8080/sql/restore/${date.name}/${type}`).subscribe(
+              (response) => {
+                // handle success
+                console.log(response);
+              },
+              (error) => {
+                // handle error
+                console.log(error);
+              }
+            );
+          })
+        }
+      });
+  }
+     
 }
+
+
+
+
+
 
 
 
